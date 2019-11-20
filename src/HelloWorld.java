c3403437579ccc197d5fdbf52e1fd47bfd8a100d
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-20 13:48:38 -0600

    Completed exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,41 @@ public class HelloWorld {
         public static void main(String[] args) {
             System.out.print("Hello,");
             System.out.print(" World!");
+
+            int myFavoriteNumber = 5;
+            System.out.println(myFavoriteNumber);
+
+            String myString = "I am learning Java.";
+            System.out.println(myString);
+
+            double myNumber = 3.14;
+            System.out.println(myNumber);
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+
+//            int three = (int) "three";
+
+//            int x = 4;
+//            x += 4;
+//
+//            int x = 3;
+//            int y = 4;
+//            y *= x;
+
+            int x = 10;
+            int y = 2;
+            x /= y;
+            y -= x;
         }
 
 }
