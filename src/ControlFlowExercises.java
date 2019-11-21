393928d91d8e971655b0b228486db39a5539c5ab
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-21 14:20:24 -0600

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
@@ -10,17 +11,37 @@ public class ControlFlowExercises {
             i++;
         }
 
-        int count = 100;
-        do {
-            System.out.printf("%d%n", count);
-            count -= 5;
-        } while (count > -10);
+//        int count = 100;
+//        do {
+//            System.out.printf("%d%n", count);
+//            count -= 5;
+//        } while (count > -10);
 
-        int square = 2;
-        do {
+        for (int count = 100; count > -10; count -= 5){
+            System.out.printf("%d%n", count);
+        }
+
+//        int square = 2;
+//        do {
+//            System.out.printf("%d%n", square);
+//            square *= 2;
+//        } while (square < 1000000);
+
+        for (int square = 2; square < 1000000; square *= 2){
             System.out.printf("%d%n", square);
-            square *= 2;
-        } while (square < 1000000);
+        }
+
+        for (int num = 1; num <= 100; num++){
+            if (num % 3 == 0 && num % 5 == 0){
+                System.out.printf("%d : FizzBuzz %n", num);
+            } else if (num % 3 == 0){
+                System.out.printf("%d : Fizz %n", num);
+            } else if (num % 5 == 0){
+                System.out.printf("%d : Buzz %n", num);
+            } else {
+                System.out.println("The number is neither fizz nor buzz");
+            }
+        }
     }
 
 }
