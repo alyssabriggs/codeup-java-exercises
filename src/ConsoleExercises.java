d6bffcd7ba0dd001bc21fbcd16880fec9a0a5a97
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-21 11:33:04 -0600

    Completed exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class ConsoleExercises {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
         double pi = 3.14159;
+
 
         System.out.format("The value of pi is approximately %.2f.%n", pi);
 
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter an integer");
         int userInput = scanner.nextInt();
@@ -18,26 +20,27 @@ public class ConsoleExercises {
         String word1 = scanner.next();
         String word2 = scanner.next();
         String word3 = scanner.next();
+
+
+
+        System.out.printf("You entered %s %s %s%n", word1, word2, word3);
+
         scanner.nextLine();
-
-
-        System.out.println("You entered " + word1 + " " + word2 + " " + word3 + ".\n");
-
 
         System.out.println("Please enter a sentence.");
         String sentence = scanner.nextLine();
 
-        System.out.println(sentence);
+        System.out.printf("You entered: %s%n", sentence);
 
 
         System.out.println("Please enter the length and width of a Codeup classroom.");
         String width = scanner.nextLine();
-        int w = Integer.parseInt(width);
+        float w = Float.parseFloat(width);
         String length = scanner.nextLine();
-        int l = Integer.parseInt(length);
+        float l = Float.parseFloat(length);
 
-        System.out.println("The perimeter of the classroom is " + ((2 * w) + (2 * l)));
-        System.out.println("The area of the classroom is " + (l * w));
+        System.out.printf("The perimeter of the classroom is: %.2f units.%n", 2 * w + 2 * l);
+        System.out.printf("The area of the classroom is: %.2f units squared.", w * l);
     }
 
 }
