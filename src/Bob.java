f4cf094fc18f2aed543f14d7ea979b34efb451be
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-22 11:42:29 -0600

    Completed exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,3 +1,4 @@
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 public class Bob {
@@ -5,22 +6,30 @@ public class Bob {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
-        String input = sc.next();
 
-        if (input.endsWith("?")){
-            System.out.println("Sure");
-        }
+        System.out.println("This is Bob.");
+        System.out.println("Let's start by greeting him");
 
-        else if (input.endsWith("!")){
-            System.out.println("Whoa, chill out!");
-        }
+        String userInput;
+        boolean greeting;
 
-        else if (input.equals("")){
-            System.out.println("Fine, be that way!");
-        }
-         else {
-             System.out.println("Whatever");
-        }
+        do {
+            userInput = sc.nextLine();
+            greeting = (!userInput.equalsIgnoreCase("bye"));
+
+
+            if (userInput.endsWith("?")){
+                System.out.println("Sure");
+            } else if (userInput.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.trim().equals("")){
+                System.out.println("Fine, be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
+
+        } while (greeting);
+
 
     }
 }
