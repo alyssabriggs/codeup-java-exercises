c84c48292e895da675515ed9aff43829584d48f8
alyssalanae <briggs.l.alyssa@gmail.com>
2019-12-04 10:25:58 -0600

    Madlibs

---
+++ src/madlibs/RunMadlibs.java
@@ -0,0 +1,19 @@
+package madlibs;
+
+public class RunMadlibs {
+    public static void main(String[] args) {
+        String[] words = {"Fer", "pink", "shirt", "The Shirt Store"};
+
+        VersionOne versionOne = new VersionOne(words);
+        System.out.println("Once upon a time...");
+        System.out.println("There was a person named...");
+        System.out.println(versionOne.name);
+        System.out.println(versionOne.goToLocation());
+        System.out.println("He saw the manager and said...");
+        System.out.println(versionOne.greeting());
+        System.out.println("\"I would like to buy that \"");
+        System.out.println(versionOne.color + " " + versionOne.randomObject);
+        System.out.println(versionOne.pickUpObject());
+        System.out.println("Then he left.");
+    }
+}
